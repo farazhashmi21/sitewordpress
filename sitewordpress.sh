1e76ade6f165521620229fb0a9df4a969ed46a67
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-03-14 18:15:39 +0000

    The Project: .htaccess File Updated.

--- sitewordpress.sh
+++ sitewordpress.sh
@@ -1,7 +1,7 @@
 echo "Site Wordpress";
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "The Project");
+echo $(git commit -m "The Project: .htaccess File Updated.");
 echo $(git push origin master);
 echo $(git remote -v);
 echo "Repository: Site Wordpress Updated.";
