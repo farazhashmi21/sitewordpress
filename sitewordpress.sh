c914fc38f9e4d8a2525be3d326648502a851f6c9
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-03-26 14:41:34 +0000

    The Project: .htaccess file has been configured to plain urls.

--- sitewordpress.sh
+++ sitewordpress.sh
@@ -1,7 +1,7 @@
 echo "Site Wordpress";
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "The Project: Wp-Cli.phar file was added to the project.");
+echo $(git commit -m "The Project: .htaccess file has been configured to plain urls.");
 echo $(git push origin master);
 echo $(git remote -v);
 echo "Repository: Site Wordpress Updated.";
