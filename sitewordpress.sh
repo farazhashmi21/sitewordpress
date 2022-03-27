60dbccb869d6bb6ec9bffea0f696394df9f8141e
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-03-27 16:45:10 +0000

    The Project: Default WordPress Project Restored With Woocommerce Plugin and Store Front Theme added.

--- sitewordpress.sh
+++ sitewordpress.sh
@@ -1,7 +1,7 @@
 echo "Site Wordpress";
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "The Project: .htaccess file has been configured to plain urls.");
+echo $(git commit -m "The Project: Default WordPress Project Restored With Woocommerce Plugin and Store Front Theme added.");
 echo $(git push origin master);
 echo $(git remote -v);
 echo "Repository: Site Wordpress Updated.";
