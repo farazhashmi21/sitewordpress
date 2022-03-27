cdf703c6e946578aa8d01e1213f8c4dee91c1a0e
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-03-27 17:10:12 +0000

    The Project: Default WordPress Project Restored With Woocommerce Plugin and Store Front and Twenty Ten Theme added.

--- sitewordpress.sh
+++ sitewordpress.sh
@@ -1,8 +1,8 @@
 echo "Site Wordpress";
+echo $(git gc);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "The Project: Default WordPress Project Restored With Woocommerce Plugin and Store Front Theme added.");
+echo $(git commit -m "The Project: Default WordPress Project Restored With Woocommerce Plugin and Store Front and Twenty Ten Theme added.");
 echo $(git push origin master);
 echo $(git remote -v);
 echo "Repository: Site Wordpress Updated.";
-
