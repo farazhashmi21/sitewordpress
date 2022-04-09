4bc498b2764bb92597ca4fb8067febd13d99423f
DeveloperFaraz6465 <farazhashmi9000@gmail.com>
2022-04-09 18:25:23 +0000

    The Project: Default WordPress Project Upgraded TO Latest WordPress Version and Advance Custom FIeld Plugins Have Been Added Into The Project.

--- sitewordpress.sh
+++ sitewordpress.sh
@@ -3,4 +3,5 @@ echo $(git gc);
 echo $(git init);
 echo $(git add .);
 echo $(git commit -m "The Project: Default WordPress Project Upgraded TO Latest WordPress Version and Advance Custom FIeld Plugins Have Been Added Into The Project.");
+echo $(git push origin master);
 echo "Repository: Site Wordpress Updated.";
